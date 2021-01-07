310d408330687430a6b23ff47ad9ee41c1d3b48e
Michael Blank <michaelblank14@gmail.com>
2021-01-07 14:24:05 -0500

    First commit

---
+++ Chat Chat iOS13/AppDelegate.swift
@@ -0,0 +1,33 @@
+
+import UIKit
+import Firebase
+import IQKeyboardManagerSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(db)
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
+
+}
+
--- Podfile
+++ Podfile
@@ -1,9 +1,9 @@
 platform :ios, '13.1'
 
-target 'Flash Chat iOS13' do
+target 'Chat Chat iOS13' do
   use_frameworks!
 
-  # Pods for Flash Chat iOS13
+  # Pods for Chat Chat iOS13
   pod 'Firebase/Auth'
   pod 'Firebase/Firestore'
 
